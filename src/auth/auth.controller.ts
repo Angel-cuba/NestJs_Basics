8ad596c2c5fed30879f834dee6a55122ddf06e70
Mac_Developer <araozangel842@gmail.com>
2022-08-30 11:57:50 +0300

    Structure and Pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { AuthBody } from './body';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body('email') email: string, @Body('password') password: string) {
+    console.log({
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password,
+    });
     return this.authService.signup();
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { User, BookMark } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   login() {
     return 'First service login';
   }
