5736516dd419130e4467917da72fa2135b32279a
Mac_Developer <araozangel842@gmail.com>
2022-08-30 21:36:29 +0300

    SignIn and SignUp. New scripts tools

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,33 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, BookMark } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  login() {
-    return 'First service login';
+  async login(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if doesn't exist throw error
+    if (!user) throw new ForbiddenException('Invalid credentials');
+
+    // if exists, check the password
+    const valid = await argon.verify(user.hash, dto.password);
+    // if password is wrong throw error'
+    if (!valid) throw new ForbiddenException('Invalid credentials');
+    // if password is correct, return the user
+    delete user.hash;
+    return user;
   }
 
   async signup(dto: AuthDto) {
     //generate the password hash
     const hash = await argon.hash(dto.password);
     // save the new user in the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      // select: {
-      //   id: true,
-      //   email: true,
-      //   createdAt: true,
-      // },
-    });
-    delete user.hash;
-    // return the saved user
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
 }
